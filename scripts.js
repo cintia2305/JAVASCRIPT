aa6023a4aad6849ce6a715dc4c29356ff15e637c
Cintia Souza <cintiacpereirapereiraesouza@gmail.com>
2025-08-08 11:16:56 -0300

    aula 8 de agosto
    aula 1

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function mudarMensagem() {
+    const men = Document.getElementById("mensagem");
+    men.innerHTML = "Você acabou de GANHAR 5% de descontos em todos produtos do site! <br> Aproveite.";
+}
